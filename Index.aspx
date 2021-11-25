da990b250a3dcf540db430dae1dddd05beb32f08
Basim Raza <basimraza17@gmail.com>
2021-11-25 07:47:24 +0500

    Country and Cities app with their codes

---
+++ Index.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="CountryCityApp.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Country APP</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+    <form id="form1" runat="server">
+
+
+        <div class="p-3 mb-2 bg-secondary text-white h4">Country and City Codes</div>
+
+        <div>
+
+            <table class="table table-borderless">
+                <tr>
+                    <td>
+                        <asp:TextBox ID="CountryCodeTextBox" runat="server" placeholder="Country Code"></asp:TextBox></td>
+                    <td class="mr-2 d-flex justify-content-end">
+                        <asp:Button ID="FindButton" runat="server" Text="Find" OnClick="FindButton_Click" />
+
+                        <asp:Button ID="SaveButton" runat="server" Text="Save" OnClick="SaveButton_Click" />
+
+                        <asp:Button ID="DeleteButton" runat="server" Text="Delete" OnClick="DeleteButton_Click" />
+
+                        <asp:Button ID="ClearButton" runat="server" Text="Clear" OnClick="ClearButton_Click" /></td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="ErrorLabel1" runat="server" Text="" ForeColor="Red"></asp:Label></td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:TextBox ID="CountryNameTextBox" runat="server" placeholder="Country Name"></asp:TextBox></td>
+                    <td>
+                        <asp:Label ID="CountryNameLabel" runat="server" Text="" ForeColor="Red"></asp:Label></td>
+
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:TextBox ID="CityCodeTextBox" runat="server" placeholder="City Code"></asp:TextBox></td>
+                    <td>
+                        <asp:Label ID="CityCodeLabel" runat="server" Text="" ForeColor="Red"></asp:Label></td>
+
+                </tr>
+                <tr>
+                    <td>
+                        <asp:TextBox ID="CityNameTextBox" runat="server" placeholder="City Name"></asp:TextBox></td>
+                    <td>
+                        <asp:Label ID="CityNameLabel" runat="server" Text="" ForeColor="Red"></asp:Label></td>
+
+                </tr>
+            </table>
+            <asp:GridView ID="CityGridView" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" Class="m-3">
+                <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+                <EditRowStyle BackColor="#999999" />
+                <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+            </asp:GridView>
+        </div>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CountryCityApp
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Use pk, ksa, ir or aus COUNTRY CODE to view city table
+
+        protected void FindButton_Click(object sender, EventArgs e)
+        {
+
+            var countryCode = CountryCodeTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(countryCode))
+            {
+                ErrorLabel1.Text = "Invalid Country Code!";
+                CountryNameTextBox.Text = "";
+
+            }
+            else
+            {
+                ErrorLabel1.Text = "";
+                var countryName = GetCountryName(countryCode);
+                if (string.IsNullOrWhiteSpace(countryName))
+                {
+                    ErrorLabel1.Text = "No Country Found With Code " + countryCode;
+                    CountryNameTextBox.Text = "";
+
+                }
+                else
+                {
+                    ErrorLabel1.Text = "";
+                    CountryNameTextBox.Text = countryName;
+
+                    DataTable cities =  GetCities(countryCode);
+                    CityGridView.DataSource = cities;
+                    CityGridView.DataBind();
+                }
+
+
+            }
+
+            CountryNameLabel.Text = "";
+            CityCodeLabel.Text = "";
+            CityNameLabel.Text = "";
+            CityCodeTextBox.Text = "";
+            CityNameTextBox.Text = "";
+        }
+
+        private DataTable GetCities(string countryCode)
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+
+            //declare @CountryCode varchar(10) = 'PK';
+
+            var query = "select CityCode as [City Code], CityName as[City Name] from City where CountryCode = @CountryCode";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter(query, con))
+                {
+                    da.SelectCommand.Parameters.AddWithValue("@CountryCode", countryCode);
+
+                    using (DataTable dt = new DataTable())
+                    {
+                        da.Fill(dt);
+
+                        if (dt != null && dt.Rows.Count > 0)
+                        {
+                            return dt;
+                        }
+                    }
+                }
+            }
+            return new DataTable();
+        }
+
+        private string GetCountryName(string countryCode)
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+
+            //declare @CountryCode varchar(10) = 'PK';
+
+            var query = "select CountryName from Country where CountryCode = @CountryCode";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter(query, con))
+                {
+                    da.SelectCommand.Parameters.AddWithValue("@CountryCode", countryCode);
+
+                    using (DataTable dt = new DataTable())
+                    {
+                        da.Fill(dt);
+
+                        if (dt != null && dt.Rows.Count > 0)
+                        {
+                            var countryName = dt.Rows[0]["CountryName"].ToString();
+                            return countryName;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            CityCodeLabel.Text = "";
+            bool check = IsAllRequiredDataGiven();
+
+            if (check)
+            {
+                (string countryCode, string countryName, string cityCode, string cityName) formData = GetFormData();
+
+                bool exist = IsCountryExist(formData.countryCode);
+
+                if (exist)
+                {
+                    SaveCity(formData);
+                }
+                else
+                {
+                    SaveCountry(formData);
+                    SaveCity(formData);
+
+                }
+
+            }
+        }
+
+        private void SaveCountry((string countryCode, string countryName, string cityCode, string cityName) formData)
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+            var query = "insert into Country values (@CountryCode, @CountryName);";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand sqlcom = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    sqlcom.Parameters.AddWithValue("@CountryCode", formData.countryCode);
+                    sqlcom.Parameters.AddWithValue("@CountryName", formData.countryName);
+
+
+                    sqlcom.ExecuteNonQuery();
+
+                }
+            }
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "AlertMessage", "alert('Country Registered Successfully')", true);
+        }
+
+        private void SaveCity((string countryCode, string countryName, string cityCode, string cityName) formData)
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+            var query = "insert into City values(@CityCode, @CityName, @CountryCode)";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand sqlcom = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    sqlcom.Parameters.AddWithValue("@CityCode", formData.cityCode);
+                    sqlcom.Parameters.AddWithValue("@CityName", formData.cityName);
+                    sqlcom.Parameters.AddWithValue("@CountryCode", formData.countryCode);
+
+                    sqlcom.ExecuteNonQuery();
+                    
+                }
+            }
+            CityCodeLabel.Text = "";
+            CityNameLabel.Text = "";
+            CityCodeTextBox.Text = "";
+            CityNameTextBox.Text = "";
+
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "AlertMessage", "alert('City Registered Successfully')", true);
+
+            DataTable cities = GetCities(formData.countryCode);
+            CityGridView.DataSource = cities;
+            CityGridView.DataBind();
+        }
+
+        private bool IsCountryExist(string countryCode)
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+            var query = "select Top 1 Cast(1 as bit) from Country where CountryCode = @CountryCode";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlDataAdapter da = new SqlDataAdapter(query, con))
+                {
+                    da.SelectCommand.Parameters.AddWithValue("@CountryCode", countryCode);
+
+                    using (DataTable dt = new DataTable())
+                    {
+                        da.Fill(dt);
+
+                        if (dt != null && dt.Rows.Count > 0)
+                        {
+                            var exist = Convert.ToBoolean(dt.Rows[0][0]);
+                            return exist;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool IsAllRequiredDataGiven()
+        {
+
+            (string countryCode, string countryName, string cityCode, string cityName ) formData = GetFormData();
+
+            bool check = true;
+            if (string.IsNullOrWhiteSpace(formData.countryCode))
+            {
+                ErrorLabel1.Text = "* Required";
+                check = false;
+            }
+            if (string.IsNullOrWhiteSpace(formData.countryName))
+            {
+                CountryNameLabel.Text = "* Required";
+                check = false;
+            }
+            if (string.IsNullOrWhiteSpace(formData.cityCode))
+            {
+                CityCodeLabel.Text = "* Required";
+                check = false;
+            }
+            if (string.IsNullOrWhiteSpace(formData.cityName))
+            {
+                CityNameLabel.Text = "* Required";
+                check = false;
+            }
+
+            return check;
+        }
+
+        private (string, string, string, string) GetFormData()
+        {
+            //var countryCode = CountryCodeTextBox.Text;
+            //var countryName = CountryNameTextBox.Text;
+            //var cityCode = CityCodeTextBox.Text;
+            //var cityName = CityNameTextBox.Text;
+
+            var formData = (CountryCodeTextBox.Text, CountryNameTextBox.Text,
+                CityCodeTextBox.Text, CityNameTextBox.Text);
+
+            return formData;
+        }
+
+        protected void DeleteButton_Click(object sender, EventArgs e)
+        {
+            (string countryCode, string countryName, string cityCode, string cityName) formData = GetFormData();
+
+            bool exist = IsCountryExist(formData.countryCode);
+
+            if (exist)
+            {
+                CountryNameLabel.Text = "";
+                CityCodeLabel.Text = "";
+
+                bool cityExist = IsCityExist(formData.cityCode);
+
+
+                if (cityExist)
+                {
+                    CityNameLabel.Text = "";
+
+                    DeleteCity(formData);
+                    CityCodeTextBox.Text = "";
+                    CityNameTextBox.Text = "";
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "AlertMessage", "alert('City Deleted Successfully')", true);
+                    DataTable cities = GetCities(formData.countryCode);
+                    CityGridView.DataSource = cities;
+                    CityGridView.DataBind();
+
+                }
+                else
+                {
+                    CityNameLabel.Text = "City doesn't exist of code " + formData.cityCode;
+                }
+            }
+            else
+            {
+                CountryNameLabel.Text = "Country doesn't exist ";
+            }
+        }
+
+        private void DeleteCity((string countryCode, string countryName, string cityCode, string cityName) formData)
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+            var query = "delete from City where CountryCode = @CountryCode and CityCode = @CityCode";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand sqlcom = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    sqlcom.Parameters.AddWithValue("@CountryCode", formData.countryCode);
+                    sqlcom.Parameters.AddWithValue("@CityCode", formData.cityCode);
+                    
+                    sqlcom.ExecuteNonQuery();
+
+                }
+            }
+        }
+
+        private bool IsCityExist(string cityCode)
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+            var query = "select Top 1 Cast(1 as bit) from City where CityCode = @CityCode";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlDataAdapter da = new SqlDataAdapter(query, con))
+                {
+                    da.SelectCommand.Parameters.AddWithValue("@CityCode", cityCode);
+
+                    using (DataTable dt = new DataTable())
+                    {
+                        da.Fill(dt);
+
+                        if (dt != null && dt.Rows.Count > 0)
+                        {
+                            var exist = Convert.ToBoolean(dt.Rows[0][0]);
+                            return exist;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        protected void ClearButton_Click(object sender, EventArgs e)
+        {
+            CountryCodeTextBox.Text = "";
+            CountryNameTextBox.Text = "";
+            CityCodeTextBox.Text = "";
+            CityNameTextBox.Text = "";
+
+            ErrorLabel1.Text = "";
+            CountryNameLabel.Text = "";
+            CityCodeLabel.Text = "";
+            CityNameLabel.Text = "";
+
+            CityGridView.DataSource = "";
+            CityGridView.DataBind();
+        }
+
+        
+    }
+}
